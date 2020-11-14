81dbeff32151d994bdb185edeacc80b767bbd2d4
Lensco <lensco@gmail.com>
2020-11-14 12:21:41 +0100

    Repository setup

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# notion-enhancer-tweaks
-Collection of tweaks to load into notion-enhancer
+# Notion Enhancer Tweaks
+Collection of tweaks and user styles for the Desktop version of Notion, loaded into notion-enhancer. You don't need to run this entire project, you can just get the generated CSS file and load it into notion-enhancer.
+
+This project consists of:
+- A Stylus file, and its compiled result `notion-enhancer-tweaks.css`
+- A demo page written in Pug, to quickly test the styles, and its compiled result `notion-enhancer-tweaks-demo.html`
+- A Gulp pipeline to generate the files and livereload everything
+
+
+## Requirements
+- NPM
+- Gulp
+
+## First run
+    $ npm install
+
+## Build
+    $ gulp
+    
+## Run from simple php server
+    php -S localhost:8000   
---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const { src, dest, watch, series, parallel } = require('gulp');
+const stylus = require('gulp-stylus');
+const pug = require('gulp-pug');
+const livereload = require('gulp-livereload');
+const files = {
+    templatesPath: '*.pug',
+    stylesPath: '*.styl'
+}
+
+function templates() {
+	return src(files.templatesPath)
+		.pipe(pug({
+			pretty: '\t'
+		}))
+		.pipe(dest('.'))
+		.pipe(livereload({
+			quiet: true
+		}));
+}
+
+function styles() {
+	return src(files.stylesPath)
+		.pipe(stylus({
+			compress: true
+		}))
+		.pipe(dest('.'))
+		.pipe(livereload({
+			quiet: true
+		}));
+}
+
+function watchTask() {
+	watch(
+		[files.templatesPath, files.stylesPath],
+		parallel(templates, styles)
+	);
+}
+
+
+exports.default = series(parallel(templates, styles), watchTask);
